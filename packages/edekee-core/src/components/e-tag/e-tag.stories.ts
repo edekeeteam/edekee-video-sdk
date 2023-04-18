1f042e6e668ffce47222c24934a28568a1ff019b
Martin-Luther Obahor <kingm4159@gmail.com>
2023-04-18 20:06:32 +0100

    added storybook and nx

---
+++ packages/edekee-core/src/components/e-tag/e-tag.stories.ts
@@ -0,0 +1,14 @@
+export default {
+  title: 'Components/Tag',
+};
+
+const Template = () => `<e-tag></e-tag>`;
+
+export const closed = Template.bind({});
+closed.args = {
+  // first: 'Winnie',
+  // middle: 'The',
+  // last: 'Pooh'
+};
+
+export const opened = Template.bind({});
